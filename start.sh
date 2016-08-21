b50807fce43b0fef46b701e36dc0682d0608763e
Vimi <admin@loveav.me>
2016-08-21 16:45:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run  -e ADMIN_PASS=123qwe -e VIRTUAL_PORT=9091 -e VIRTUAL_HOST=transmission.mini.25752.com,transmission.inter.25752.com -v /data/volume/downloads/transmission:/opt/transmission/downloads -v /data/volume/downloads/incomplete:/opt/transmission/incomplete -d --name=transmission --restart=always --privileged transmission
+docker run  -e ADMIN_PASS=123qwe -e VIRTUAL_PORT=9091 -e VIRTUAL_HOST=transmission.mini.25752.com,transmission.inter.25752.com -v /data/volume/downloads/transmission:/opt/transmission/downloads -v /data/volume/downloads/incomplete:/opt/transmission/incomplete -d --name=root --restart=always --privileged transmission
